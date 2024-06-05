b279828641a6101a2530648a9bf321887d69785c
smelik81 <artem.smelyanskiy81@gmail.com>
2024-06-05 16:50:58 +0300

    change

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -111,19 +111,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
      backdrop.addEventListener('click', closeModal);
-
-    function checkElements() {
-      const main = document.querySelector('main');
-      const header = document.querySelector('header');
-      const footer = document.querySelector('footer');
-
-      if (main && header && footer) {
-        document.querySelectorAll('main, header, footer').forEach(el => {
-          el.classList.add('disable-interaction');
-        });
-      }
-    }
-    checkElements(); 
     
     const iziToastConfig = {
         position: 'topRight',
