efb392a1f84b7cf7d83db78ff28d339da9a342a1
smelik81 <artem.smelyanskiy81@gmail.com>
2025-01-09 02:57:08 +0200

    change config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => {
   return {
+    base: command === 'build' ? '/portfolio/' : '/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
